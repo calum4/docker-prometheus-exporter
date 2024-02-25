694c9aff8b51d038e240194800114f4d183daa15
Calum <calum@calumm.dev>
2024-02-25 20:50:55 +0000

    Axum Init

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
-use std::convert::Infallible;
 use std::net::SocketAddr;
 use std::sync::Arc;
+use axum::{Router, serve};
+use axum::http::StatusCode;
+use axum::routing::get;
 use docker_api::Docker;
-use http_body_util::Full;
-use hyper::body::{Body, Bytes, Incoming};
-use hyper::{http, Request, Response};
-use hyper::header::CONTENT_TYPE;
-use hyper::server::conn::http1;
-use hyper::service::service_fn;
-use hyper_util::rt::TokioIo;
 use prometheus::{Encoder, TextEncoder};
 use tokio::net::TcpListener;
 use tokio::signal;
-use tracing::{error, info, instrument};
+use tracing::{error, info};
 use tracing_subscriber::EnvFilter;
 use tracing_subscriber::fmt::format::FmtSpan;
 use crate::config::CONFIG_ENV;
@@ -31,9 +26,7 @@ async fn main() {
         .with_span_events(FmtSpan::CLOSE)
         .init();
 
-    if let Err(error) = start_http_server().await {
-        panic!("{error}");
-    }
+    start_http_server().await;
 
     let docker = {
         let host = match &CONFIG_ENV.docker_host {
@@ -61,48 +54,40 @@ async fn main() {
     signal::ctrl_c().await.expect("Failed to listen for CTRL+C");
 }
 
-async fn start_http_server() -> tokio::io::Result<()> {
+async fn start_http_server() {
     let addr = SocketAddr::from(([127, 0, 0, 1], 9000));
-    let listener = TcpListener::bind(addr).await?;
 
-    info!("Listening on http://{addr}");
+    let listener = match TcpListener::bind(addr).await {
+        Ok(listener) => listener,
+        Err(error) => panic!("{error}"),
+    };
+
+    let router = Router::new()
+        .route("/", get(serve_metrics))
+        .route("/metrics", get(serve_metrics));
 
     tokio::spawn(async move {
-        loop {
-            let (stream, addr) = match listener.accept().await {
-                Ok((stream, addr)) => (stream, addr),
-                Err(error) => {
-                    error!("Errored when receiving connection! {error}");
-                    continue
-                }
-            };
-
-            let io = TokioIo::new(stream);
-
-            tokio::spawn(async move {
-                if let Err(error) = http1::Builder::new().serve_connection(io, service_fn(serve_req)).await {
-                    error!(addr=addr.to_string(), "Errored when serving connection! {error}");
-                }
-            });
+        if let Err(error) = serve(listener, router).await {
+            panic!("{error}");
         }
-    });
 
-    Ok(())
+        info!("Listening on http://{addr}");
+    });
 }
 
-// async fn serve_req(_: Request<Incoming>) -> Result<Response<Full<Bytes>>, http::Error> {
-//     let encoder = TextEncoder::new();
-//
-//     let metric_families = prometheus::gather();
-//     let mut buffer = vec![];
-//     encoder.encode(&metric_families, &mut buffer).unwrap();
-//
-//     Response::builder()
-//         .status(200)
-//         .header(CONTENT_TYPE, encoder.format_type())
-//         .body(Full::from(Body::from(buffer)))
-// }
+// TODO - Instrument with client ip and req path
+async fn serve_metrics() -> Result<String, StatusCode> {
+    let encoder = TextEncoder::new();
 
-async fn serve_req(req: Request<Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
-    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
+    let metric_families = prometheus::gather();
+    let mut buffer = vec![];
+    encoder.encode(&metric_families, &mut buffer).unwrap();
+
+    match String::from_utf8(buffer) {
+        Ok(string) => Ok(string),
+        Err(error) => {
+            error!("{error}");
+            Err(StatusCode::INTERNAL_SERVER_ERROR)
+        }
+    }
 }
