e064936f2efa3d8a9cbd7164e5957b36c61a5de5
Calum <calum@calumm.dev>
2025-03-25 19:34:32 +0000

    refactor: cargo fmt

--- src/config.rs
+++ src/config.rs
@@ -16,8 +16,12 @@ pub(crate) fn get_config() -> &'static Config {
     fn construct() -> Config {
         Config {
             docker_host: env::var("DOCKER_HOST").ok(),
-            listen_addr: env::var("LISTEN_ADDR").map(|addr| IpAddr::from_str(addr.as_str()).expect("Invalid LISTEN_ADDR provided")).unwrap_or(IpAddr::V4(Ipv4Addr::UNSPECIFIED)),
-            listen_port: env::var("LISTEN_PORT").map(|port| u16::from_str(port.as_str()).expect("Invalid LISTEN_PORT provided")).unwrap_or(9000),
+            listen_addr: env::var("LISTEN_ADDR")
+                .map(|addr| IpAddr::from_str(addr.as_str()).expect("Invalid LISTEN_ADDR provided"))
+                .unwrap_or(IpAddr::V4(Ipv4Addr::UNSPECIFIED)),
+            listen_port: env::var("LISTEN_PORT")
+                .map(|port| u16::from_str(port.as_str()).expect("Invalid LISTEN_PORT provided"))
+                .unwrap_or(9000),
         }
     }
 
--- src/metrics/up.rs
+++ src/metrics/up.rs
@@ -1,9 +1,9 @@
-use std::sync::Arc;
-use std::time::Duration;
+use crate::metrics::Metric;
 use docker_api::Docker;
 use prometheus::{IntGauge, register_int_gauge};
+use std::sync::Arc;
+use std::time::Duration;
 use tracing::instrument;
-use crate::metrics::Metric;
 
 pub(crate) struct UpMetric {
     metric: IntGauge,
